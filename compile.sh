50b0dc1de370616ea04459e25a36d16b2c7ac70f
Edgar Marko Trono <etrono@Edgars-MacBook-Pro.local>
2018-08-02 22:41:18 +0900

    Added integer sorter

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-clang++ -std=c++11 -stdlib=libc++ main.cpp random.cpp output.cpp -o sort.out
+clang++ -std=c++11 -stdlib=libc++ main.cpp random.cpp output.cpp sorters.cpp -o sort.out
--- random.cpp
+++ random.cpp
@@ -1,15 +1,15 @@
 #include<random>
 
-int random_int() {
+int random_int(int min, int max) {
   std::random_device rd_int;
   std::mt19937 engine_int(rd_int());
-  std::uniform_int_distribution<int> dist_int(0,99999);
+  std::uniform_int_distribution<int> dist_int(min,max);
   return dist_int(engine_int);
 }
 
-double random_double() {
+double random_double(double min, double max) {
   std::random_device rd_double;
   std::mt19937 engine_double(rd_double());
-  std::uniform_real_distribution<double> dist_double(0.0,1.0);
+  std::uniform_real_distribution<double> dist_double(min, max);
   return dist_double(engine_double);
 }
